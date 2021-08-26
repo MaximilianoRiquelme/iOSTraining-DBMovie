bdfb0fb97688cc571fe33e9b505be7a8685b4a23
Maximiliano Riquelme Vera <maximiliano.riquelme@globant.com>
2021-08-26 10:29:44 -0300

    Project cloned

---
+++ TheMovieDB/TheMovieDB/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Jaime Laino on 8/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
